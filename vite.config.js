3d3ee589f576126f35b2438f1d317ab235b7710d
Radji Mohamed <radjimohamed013@gmail.com>
2025-06-07 16:20:05 +0100

    configuration du fichier vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,5 +7,17 @@ export default defineConfig({
   preview: {
     port: 4173,
     host: '0.0.0.0'
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
   }
 })
